0c43b8bcc371a8777b9385abc8b6783fa6d5f64f
barry <barry.sia@gmail.com>
2012-03-25 10:47:55 -0700

    Genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DayCounter::Application.config.secret_token = '9fdc55ed43742bea295015d297b1ca741ae9a91e9432b361a55e3b55f3dc4b12ba9fe65aed558c5b20fed88c4719211a97b50308c0db49401fdd0664f6b272f0'
